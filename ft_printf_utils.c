b0793c8aabd14762ced09ef0aab57ccec19f5dee
Mouad Mezyan <mmezyan@e2r7p6.1337.ma>
2023-11-27 15:22:38 +0100

    ft_printf

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,12 +6,11 @@
 /*   By: mmezyan <mmezyan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:28:48 by mmezyan           #+#    #+#             */
-/*   Updated: 2023/11/27 14:36:57 by mmezyan          ###   ########.fr       */
+/*   Updated: 2023/11/27 15:22:13 by mmezyan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 int	ft_putchar(char c)
 {
@@ -33,54 +32,6 @@ int	ft_putstr(char *str)
 		i++;
 	}
 	return (i);
-}
-
-int	ft_put_x(unsigned int nb)
-{
-	int count;
-	char *a;
-	char *s;
-	int i;
-
-	count = 0;
-	i = ft_number_size_xX(nb);
-	a = "0123456789abcdef";
-	s = malloc(i + 1);
-	count += 2;
-	s[i] = '\0';
-	while (nb)
-	{
-		s[--i] = a[nb %16];
-		nb /= 16;
-		count ++;
-	}
-	ft_putstr(s);
-	free(s);
-	return (count);
-}
-
-int	ft_put_X(unsigned int nb)
-{
-	int count;
-	char *a;
-	char *s;
-	int i;
-
-	count = 0;
-	i = ft_number_size_xX(nb);
-	a = "0123456789ABCDEF";
-	s = malloc(ft_number_size_pointer(nb)+ 1);
-	count += 2;
-	s[i] = '\0';
-	while (nb)
-	{
-		s[--i] = a[nb %16];
-		nb /= 16;
-		count ++;
-	}
-	ft_putstr(s);
-	free(s);
-	return (count);
 }
 
 int	ft_putuns(unsigned int nb)
@@ -129,61 +80,4 @@ int	ft_putnbr(int nb)
 		count+=ft_putnbr(nb % 10);
 	}
 	return (count);
-}
-
-int	ft_putpointer(unsigned long n)
-{
-	int count;
-	char *a;
-	char *s;
-	int i;
-
-	count = 0;
-	i = ft_number_size_pointer(n);
-	a = "0123456789abcdef";
-	s = malloc(ft_number_size_pointer(n)+ 1);
-	if(write(1, "0x0", 3) == -1)
-		return (-1);
-	count += 3;
-	s[i] = '\0';
-	while (n)
-	{
-		s[--i] = a[n %16];
-		n /= 16;
-		count ++;
-	}
-	ft_putstr(s);
-	free(s);
-	return (count);
-}
-unsigned int	ft_number_size_xX(unsigned int number)
-{
-	unsigned int	length;
-
-	length = 0;
-	if (number == 0)
-		return (1);
-	if (number < 0)
-		length += 1;
-	while (number != 0)
-	{
-		number /= 10;
-		length++;
-	}
-	return (length);
-}
-
-unsigned int	ft_number_size_pointer(unsigned long number)
-{
-	unsigned int	length;
-
-	length = 0;
-	if (number == 0)
-		return (1);
-	while (number > 0)
-	{
-		number /= 16;
-		length++;
-	}
-	return (length);
 }
---
+++ ft_printf_utils2.c
@@ -0,0 +1,105 @@
+int	ft_put_x(unsigned int nb)
+{
+	int count;
+	char *a;
+	char *s;
+	int i;
+
+	count = 0;
+	i = ft_number_size_xX(nb);
+	a = "0123456789abcdef";
+	s = malloc(i + 1);
+	count += 2;
+	s[i] = '\0';
+	while (nb)
+	{
+		s[--i] = a[nb %16];
+		nb /= 16;
+		count ++;
+	}
+	ft_putstr(s);
+	free(s);
+	return (count);
+}
+
+int	ft_put_X(unsigned int nb)
+{
+	int count;
+	char *a;
+	char *s;
+	int i;
+
+	count = 0;
+	i = ft_number_size_xX(nb);
+	a = "0123456789ABCDEF";
+	s = malloc(ft_number_size_pointer(nb)+ 1);
+	count += 2;
+	s[i] = '\0';
+	while (nb)
+	{
+		s[--i] = a[nb %16];
+		nb /= 16;
+		count ++;
+	}
+	ft_putstr(s);
+	free(s);
+	return (count);
+}
+
+int	ft_putpointer(unsigned long n)
+{
+	int count;
+	char *a;
+	char *s;
+	int i;
+
+	count = 0;
+	i = ft_number_size_pointer(n);
+	a = "0123456789abcdef";
+	s = malloc(ft_number_size_pointer(n)+ 1);
+	if(write(1, "0x0", 3) == -1)
+		return (-1);
+	count += 3;
+	s[i] = '\0';
+	while (n)
+	{
+		s[--i] = a[n %16];
+		n /= 16;
+		count ++;
+	}
+	ft_putstr(s);
+	free(s);
+	return (count);
+}
+
+unsigned int	ft_number_size_xX(unsigned int number)
+{
+	unsigned int	length;
+
+	length = 0;
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		length += 1;
+	while (number != 0)
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
+}
+
+unsigned int	ft_number_size_pointer(unsigned long number)
+{
+	unsigned int	length;
+
+	length = 0;
+	if (number == 0)
+		return (1);
+	while (number > 0)
+	{
+		number /= 16;
+		length++;
+	}
+	return (length);
+}
